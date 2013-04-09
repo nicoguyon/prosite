972fb6d0634dfe7f9cd0507faa05e48c902101ef
nicoguyon <nicoguyon@gmail.com>
2013-04-09 23:36:41 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prosite::Application.config.secret_token = '05acdd9a24d8b39a1ef90bd7269c03715b5c14407b3efdfa19df581143370cf60daf0aa44e8a2c8cafac25b0e17126622b739a916f1929b494f6fa6b5cfb35d3'
